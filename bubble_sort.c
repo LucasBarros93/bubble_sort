b4b121823be20a62fe853d2d06cf18406556806b
NinjaPro93 <ninjapro93lu@gmail.com>
2024-03-26 20:20:10 -0300

    arrumei a casa e to printando a média dos tempos

--- bubble_sort.c
+++ bubble_sort.c
@@ -27,33 +27,42 @@ void main(void){
     int size, num;
     
     for(int n = 1; n <= 20; n++){
-        int x = 0;
-        char filename[10];
-        sprintf(filename, "%d.in", n);
-        FILE *fp = fopen(filename, "r");
 
-        if (fp == NULL){
-            printf("Error: could not open file %s", filename);
-            return;
+        double time_averege = 0;
+
+        for(int _ = 0; _<10; _++){
+            int x = 0;
+            char filename[13];
+            sprintf(filename, "./tests/%d.in", n);
+            FILE *fp = fopen(filename, "r");
+
+            if (fp == NULL){
+                printf("Error: could not open file %s", filename);
+                return;
+            }
+
+            fscanf(fp, "%d", &size);  // lendo apenas a primeira linha para saber quantos números o arquivo tem
+            int *arr = calloc(size, sizeof(int));
+
+            while (fscanf(fp, "%d", &num) == 1) {
+                arr[x] = num;
+                x++;
+            }
+            fclose(fp);
+
+            clock_t t = clock();
+            bubble_sort(arr, size);
+            t = clock() - t;
+
+            double time_taken = ((double)t)/CLOCKS_PER_SEC;  // calcula o tempo levado para executar a função
+
+            time_averege += time_taken;
+
+            free(arr);
         }
+        
+        printf("Tempo demorado para o arquivo %d: %lf\n", n, time_averege/10);
 
-        fscanf(fp, "%d", &size);  // lendo apenas a primeira linha para saber quantos números o arquivo tem
-        int *arr = calloc(size, sizeof(int));
-
-        while (fscanf(fp, "%d", &num) == 1) {
-            arr[x] = num;
-            x++;
-        }
-        fclose(fp);
-
-        clock_t t = clock();
-        bubble_sort(arr, size);
-        t = clock() - t;
-
-        double time_taken = ((double)t)/CLOCKS_PER_SEC;  // calcula o tempo levado para executar a função
-        printf("Tempo demorado para o arquivo %d: %lf\n", n, time_taken);
-
-        free(arr);
     }
     
 }

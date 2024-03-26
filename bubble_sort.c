77863a8af62a7f931175d184b4b88b5bf1c7f24d
NinjaPro93 <ninjapro93lu@gmail.com>
2024-03-26 19:00:49 -0300

    arrumei detalhes bobos

--- bubble_sort.c
+++ bubble_sort.c
@@ -2,8 +2,7 @@
 #include <stdlib.h>
 
 void bubble_sort (int *arr, int size) {
-    int aux;
-    int flag;
+    int aux, flag;
     for (int k = 1; k < size; k++) {
         flag = 0;
         for (int j = 0; j < size - k; j++) {
@@ -24,11 +23,9 @@ void bubble_sort (int *arr, int size) {
 
 void main(void){
 
-    int n;
-    int size;
-    int num;
+    int size, num;
     
-    for(n = 1; n <= 20; n++){
+    for(int n = 1; n <= 20; n++){
         int x = 0;
         char filename[10];
         sprintf(filename, "%d.in", n);
@@ -42,7 +39,7 @@ void main(void){
         fscanf(fp, "%d", &size);  // lendo apenas a primeira linha para saber quantos números o arquivo tem
         int *arr = calloc(size, sizeof(int));
 
-        while (fscanf(fp, "%d", &num) == 1) {
+        while (fscanf(fp, "%d", &num)) {
             arr[x] = num;
             x++;
         }
@@ -58,23 +55,4 @@ void main(void){
         free(arr);
     }
     
-}
-
-    // int tam_arq;
-
-    // FILE *arq;
-    // arq = fopen("n.i", "r");  // abrindo o arquivo
-    
-    // if (arq == NULL){  // verificando se o arquivo foi lido corretamente
-    //     printf("Erro na leitura do arquivo!\n");
-    //     exit(0);
-    // }
-
-    // for(int i = 1; i <= 20; i++){
-    //     fscanf(arq, "%d", &tam_arq);  // lendo apenas a primeira linha para saber quantos números o arquivo tem
-    //     int *vetor = calloc(tam_arq, sizeof(int));
-        
-    // }
-    
-    // return 0;
 }

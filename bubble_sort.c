5218cf51eec78aeb7732593aba565306623b8400
NinjaPro93 <ninjapro93lu@gmail.com>
2024-03-26 13:47:02 -0300

    primeira ideia pra ler o arqqquuivo

--- bubble_sort.c
+++ bubble_sort.c
@@ -15,24 +15,39 @@ void bubble_sort (int *arr, int size) {
     }
 }
 
-int main(){
+void main(void){
 
-    int tam_arq;
+    char *filename = "n.i";
+    FILE *fp = fopen(filename, "r");
 
-    FILE *arq;
-    arq = fopen("n.i", "r");  // abrindo o arquivo
-    
-    if (arq == NULL){  // verificando se o arquivo foi lido corretamente
-        printf("Erro na leitura do arquivo!\n");
-        exit(0);
+    if (fp == NULL){
+        printf("Error: could not open file %s", filename);
+        return;
     }
 
-    for(int i = 1; i <= 20; i++){
-        fscanf(arq, "%d", &tam_arq);  // lendo apenas a primeira linha para saber quantos números o arquivo tem
-        int *vetor = calloc(tam_arq, sizeof(int));
+    int num, x=0;
+    int arr[100];
+    while (fscanf(fp, "%d", num) == 1) {
+        arr[x] = num;
+        x++;
+    }
+    fclose(fp);
+
+    // int tam_arq;
+
+    // FILE *arq;
+    // arq = fopen("n.i", "r");  // abrindo o arquivo
+    
+    // if (arq == NULL){  // verificando se o arquivo foi lido corretamente
+    //     printf("Erro na leitura do arquivo!\n");
+    //     exit(0);
+    // }
+
+    // for(int i = 1; i <= 20; i++){
+    //     fscanf(arq, "%d", &tam_arq);  // lendo apenas a primeira linha para saber quantos números o arquivo tem
+    //     int *vetor = calloc(tam_arq, sizeof(int));
         
-    }
+    // }
     
-
-    return 0;
+    // return 0;
 }

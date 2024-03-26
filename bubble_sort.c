75c7ce49566e4118f45b1532488136acdb8bb22a
AndreBorba <andrevillalba2.av@gmail.com>
2024-03-26 17:43:21 -0300

    ainda falta mexer com o tempo de execução

--- bubble_sort.c
+++ bubble_sort.c
@@ -3,35 +3,62 @@
 
 void bubble_sort (int *arr, int size) {
     int aux;
+    int flag;
     for (int k = 1; k < size; k++) {
-        for (int j = 0; j < size-1; j++) {
-
+        flag = 0;
+        for (int j = 0; j < size - k; j++) {
+            
             if (arr[j] > arr[j+1]) {
                 aux      = arr[j];
                 arr[j]   = arr[j+1];
                 arr[j+1] = aux;
+                flag = 1;
             }
+        }
+
+        if(flag == 0){  // flag=0 significa que não houve mudança nenhuma no vetor, logo, ele já está ordenado.
+            break;
         }
     }
 }
 
 void main(void){
 
-    char *filename = "n.i";
-    FILE *fp = fopen(filename, "r");
+    int n;
+    int size;
+    int num;
+    
+    for(n = 1; n <= 20; n++){
+        int x = 0;
+        char filename[10];
+        sprintf(filename, "%d.in", n);
+        FILE *fp = fopen(filename, "r");
 
-    if (fp == NULL){
-        printf("Error: could not open file %s", filename);
-        return;
-    }
+        if (fp == NULL){
+            printf("Error: could not open file %s", filename);
+            return;
+        }
 
-    int num, x=0;
-    int arr[100];
-    while (fscanf(fp, "%d", num) == 1) {
-        arr[x] = num;
-        x++;
+        fscanf(fp, "%d", &size);  // lendo apenas a primeira linha para saber quantos números o arquivo tem
+        int *arr = calloc(size, sizeof(int));
+
+        while (fscanf(fp, "%d", &num) == 1) {
+            arr[x] = num;
+            x++;
+        }
+        fclose(fp);
+
+        bubble_sort(arr, size);
+
+      /*  if(n == 2){
+            for(int p = 0; p<size; p++){
+                printf("%d ", arr[p]);
+
+            }  isso aqui é só pra testar se tá ordenando*/
+        free(arr);
     }
-    fclose(fp);
+    
+}
 
     // int tam_arq;
 
